24010addc7cc7def715afd636886dfde4b7da343
brunales21 <bemimarket@gmail.com>
2024-02-02 16:54:30 +0100

    limpieza

--- src/main/java/org/example/chatbmbis/Server.java
+++ src/main/java/org/example/chatbmbis/Server.java
@@ -12,8 +12,6 @@ public class Server {
 
     private static User userNow;
     private Map<Socket, String> usersMap;
-   // private List<Channel> channels;
-    //private List<PrivateChat> privateChats;
 
 
     public Server() {
